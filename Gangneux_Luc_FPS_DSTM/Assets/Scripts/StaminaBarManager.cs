636a5eb2edb8c0f827419876097baa475d37d9d8
GangneuxLuc <l.gangneux@bdx.ecolescreatives.com>
2026-01-19 17:58:02 +0100

    Barre de stamina finalisée
    J'ai ajustée ma barre de stamina pour qu'elle soit visible quand elle se remplie.

--- Gangneux_Luc_FPS_DSTM/Assets/Scripts/StaminaBarManager.cs
+++ Gangneux_Luc_FPS_DSTM/Assets/Scripts/StaminaBarManager.cs
@@ -10,6 +10,8 @@ public class StaminaBarManager : MonoBehaviour
     float maxStamina = 100;
     float staminaRegenRate;
     public Image staminaBarImage;
+    public Image staminaBarBackgroundImage;
+    private bool isSprinting;
 
     private void Start()
     {
@@ -20,9 +22,17 @@ public class StaminaBarManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+
         stamina = playerController.GetComponent<FPS_Controller.FirstPersonController>().playerStaminaLevel;
         maxStamina = playerController.GetComponent<FPS_Controller.FirstPersonController>().playerStaminaMax;
         staminaBarImage.fillAmount = stamina / maxStamina;
-        //staminaBarImage.fillAmount = stamina / maxStamina;
+
+    
+
+        if (stamina < maxStamina)
+        { staminaBarImage.enabled = true; staminaBarBackgroundImage.enabled = true; }
+        else
+        { staminaBarImage.enabled = false; staminaBarBackgroundImage.enabled = false; }
     }
 }

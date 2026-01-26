86d04c0a839ee431c383910920b4295bdf5fb0f4
GangneuxLuc <l.gangneux@bdx.ecolescreatives.com>
2026-01-26 17:47:19 +0100

    Commit final lundi

---
+++ Gangneux_Luc_FPS_DSTM/Assets/Scripts/AiMoose.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiMoose : MonoBehaviour
+{
+    public Transform Joueur;
+    public NavMeshAgent Ennemi;
+    Collider sphereCollider;
+    Animator animator;
+    float speed = 3.5f; 
+
+
+
+    void Start()
+    {
+     
+
+    }
+
+
+}
